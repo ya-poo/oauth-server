20e1df3881b7dc37dcdd2bdf6474ea4b5267d468
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2022-10-03 20:18:46 +0900

    [ErrorResponse] Positioned for use when there is no specification in the RFC regarding response to errors

--- src/main/kotlin/me/yapoo/oauth/web/error/ErrorResponse.kt
+++ src/main/kotlin/me/yapoo/oauth/web/error/ErrorResponse.kt
@@ -1,6 +1,9 @@
 package me.yapoo.oauth.web.error
 
+/**
+ * RFC でエラーの型が定義されていない場合に用いる共通のエラーレスポンス
+ */
 data class ErrorResponse(
     val error: String,
-    val description: String,
+    val errorDescription: String,
 )
