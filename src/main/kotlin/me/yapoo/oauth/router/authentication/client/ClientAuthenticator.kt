9625a838f6e82722dbcde1d6f98b626a87db703f
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2022-10-08 21:43:15 +0900

    support Client Authentication with Authorization Header

--- src/main/kotlin/me/yapoo/oauth/router/authentication/client/ClientAuthenticator.kt
+++ src/main/kotlin/me/yapoo/oauth/router/authentication/client/ClientAuthenticator.kt
@@ -4,10 +4,9 @@ import arrow.core.continuations.nullable
 import me.yapoo.oauth.domain.client.Client
 import me.yapoo.oauth.domain.client.ClientId
 import me.yapoo.oauth.domain.client.ClientRepository
-import me.yapoo.oauth.mixin.spring.getSingle
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.awaitFormData
+import java.util.*
 
 @Component
 class ClientAuthenticator(
@@ -16,18 +15,28 @@ class ClientAuthenticator(
 ) {
 
     // クライアント認証 (RFC 6749 - 2.3)
-    // 実装が面倒なので NOT RECOMMENDED なリクエストボディを用いた認証(同 2.3.1)を行う。
+    // Authorization ヘッダによるベーシック認証のみサポートする。
+    // リクエストボディを用いた認証(同 2.3.1, NOT RECOMMENDED) はサポートしない。
     // ブルートフォースアタック対策は省略。
     suspend fun doAuthentication(
         request: ServerRequest
     ): Client? {
         return nullable {
-            val body = request.awaitFormData()
-            val clientId = body.getSingle("client_id")
-                ?.let(::ClientId)
+            val authorizationHeaderValue = request.headers().header("Authorization")
+                .singleOrNull()
                 .bind()
 
-            val clientSecret = body.getSingle("client_secret").bind()
+            ensure(authorizationHeaderValue.startsWith("Basic "))
+
+            val list = authorizationHeaderValue.substring(6)
+                .let { Base64.getDecoder().decode(it).toString() }
+                .split(":")
+
+            ensure(list.size == 2)
+
+            val clientId = list[0].let(::ClientId)
+
+            val clientSecret = list[1]
 
             val clientCredential = clientCredentialRepository.find(clientId).bind()
 
